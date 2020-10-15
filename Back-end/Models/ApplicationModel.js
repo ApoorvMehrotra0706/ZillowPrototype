1f5b20282f30870db08c40a0b18bbe4db713e632
pranjay01 <pranjay.sagar01@gmail.com>
2020-10-14 19:11:39 -0700

    Static, User, Application Models

---
+++ Back-end/Models/ApplicationModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const applicationSchema = new Schema(
+  {
+    ListingID: { type: String, required: true },
+    ApplicantID: { type: String, required: true },
+    ApplicantName: { type: String, required: true },
+    OwnerID: { type: String, required: true },
+    // OwnerName: { type: String, required: true },
+    RealtorID: { type: String },
+    // RealtorName: { type: String },
+    Status: {
+      type: String,
+      enum: ["Pending", "Accepted", "Rejected"],
+      required: true,
+      default: "Pending",
+    },
+    PriceQuoted: { type: Number, required: true },
+    Message: { type: String },
+    CreditScore: { type: Number },
+    EmpInformation: { type: String },
+  },
+  { versionKey: false }
+);
+
+userSignupSchema.index({ ApplicantID: 1, ListingID: 1 }, { unique: true });
+
+const ApplicationModel = mongoose.model("application", applicationSchema);
+module.exports = ApplicationModel;
--- Back-end/Models/UsersModel.js
+++ Back-end/Models/UsersModel.js
@@ -5,12 +5,44 @@ const { Schema } = mongoose;
 const userSchema = new Schema(
   {
     UserID: { type: String, required: true },
-    FoodName: { type: String, required: true },
-    MainIngredients: { type: String, required: true },
-    Cuisine: { type: String, required: true },
-    Description: { type: String },
-    ImageUrl: { type: String },
-    Price: { type: Number, required: true },
+    FirstName: { type: String, required: true },
+    LastName: { type: String, required: true },
+    DOB: { type: String, required: true },
+    ImageURL: { type: String },
+    Contact: { type: Number, required: true },
+    FavouriteHomes: [
+      {
+        ListingID: { type: String, required: true },
+        ImageURL: [String],
+        ZIP: { type: Number, min: 10000, max: 99999, required: true },
+        StreetAddress: { type: String },
+        Price: { type: Number, required: true },
+      },
+    ],
+    FavouriteSearches: [
+      {
+        ZIP: { type: Number, min: 10000, max: 99999 },
+        StreetAddress: { type: String },
+        PriceStartRange: { type: Number },
+        PriceEndRange: { type: Number },
+        Area: { type: Number },
+        NoOfBedrooms: { type: Number },
+        NoOfBathrooms: { type: Number },
+        FlooringType: { type: String, enum: ["Carpet", "WoodenFlooring"] },
+        HomeType: {
+          type: String,
+          enum: [
+            "Apartment",
+            "Townhome",
+            "Attached Single Family Home",
+            "Detached",
+          ],
+        },
+        Parking: { type: String, enum: ["Open", "Closed", "No"] },
+        Amenities: { type: String },
+        YearBuilt: { type: Number },
+      },
+    ],
   },
   { versionKey: false }
 );
