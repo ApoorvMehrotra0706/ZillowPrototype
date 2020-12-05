57dc06ba0e54b2d49d90572d858bd344c4add846
Pranav Karmalkar <pranavkarmalkar@gmail.com>
2020-12-04 20:36:03 -0800

    home description update

---
+++ frontend/src/action/getAppliedAction.js
@@ -0,0 +1,47 @@
+import { GET_APPLIED } from "./actionTypes";
+import configPath from "../config";
+import axios from "axios";
+
+const searchDispatcher = (payload) => {
+  console.log("Inside registerCustomerDispatcher action");
+  console.log("payload", payload);
+  return {
+    type: GET_LISTING,
+    payload,
+  };
+};
+
+//Delayed dispatch to make async call for Customer data
+export const search1 = (payload) => {
+  console.log("Inside listingAction thunk");
+
+  return (dispatch) => {
+    //set the with credentials to true
+    axios.defaults.headers.common.authorization = localStorage.getItem(
+      "IDToken"
+    );
+    //make a post request with the user data
+    axios
+      .get(configPath.api_host + "/housing/getListingByID", { params: payload })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(response.data);
+          dispatch(searchDispatcher(response.data));
+        } else {
+          dispatch(searchDispatcher(response.data));
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data);
+          dispatch(searchDispatcher(error.response.data));
+        } else {
+          dispatch(
+            searchDispatcher({
+              res: "Network error",
+            })
+          );
+        }
+      });
+  };
+};
